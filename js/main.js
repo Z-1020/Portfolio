a3e707d387d91f41634301a2f73dda563de51ae4
Z-1020 <zoemargerie14@gmail.com>
2026-01-31 19:34:24 +0100

    feat: adding home main

--- js/main.js
+++ js/main.js
@@ -1,3 +1,39 @@
+const projectsHome = [
+  {
+    title : "Application de recherche de stage",
+    color: "bg-stone-600 p-4 w-3/4 mx-auto rounded-xl shadow-lg shadow-stone-700",
+    image : "src/assets/applicationStage.png",
+    description : "Cette application a été conçue pour répondre à la demande d'une cliente (fictive), souhaitant une application afin de faciliter la recherche de stage de ses étudiants.",
+  },
+  {
+    title : "DungeonXplorer",
+    color: "bg-stone-700 p-4 w-3/4 rounded-xl mx-auto shadow-lg shadow-stone-800",
+    image : "src/assets/dungeonXplorer.png",
+    description : "DungeonXplorer est un jeu développé en PHP, il est inspiré d'un « livre dont vous êtes le héros ». Le joueur peut gérer son compte, s'inscrire, se connecter et commencer une aventure.",
+    
+  },
+]
+
+function loadHome(){
+  document.getElementById("projet").innerHTML = 
+  projectsHome.map(p=> `
+    <article>
+    <div class= "${p.color}">
+                <h2 class="text-2xl md:text-4xl font-bold text-center p-4 md:p-10">${p.title}</h2>
+                <img src=${p.image} class="mx-auto rounded-xl" alt="Capture d'écran "></img>
+                <div class="md:flex md:flex-col p-4 md:p-4">
+                  <h3 class="font-bold text-xl text-2xl md:text-4xl font-bold mt-4">Description</h3>
+                  <p  class="space-y-1 text-justify md:text-3xl text-l mt-4">${p.description}</p> 
+                  <button aria-label="Voir les projets" class="hover:cursor-pointer p-4 bg-stone-800 md:w-1/2 w-full rounded-xl m-4 hover:bg-stone-900 mx-auto text-l md:text-3xl"}>
+                    Voir les projets
+                  </button>       
+                </div>
+              </div>
+    </article>
+
+  `).join("")
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   const mobileQuery = window.matchMedia("(max-width: 767px)");
 
@@ -38,6 +74,7 @@ document.addEventListener("DOMContentLoaded", () => {
   })
 }
 loadFooter()
+loadHome()
   mobileQuery.addEventListener("change", loadHeaderAndNavbar)
   loadHeaderAndNavbar()
   
