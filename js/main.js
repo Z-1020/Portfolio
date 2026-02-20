3a9d0c5ffe182289b0122b43016fb6a9f0b45efc
Z-1020 <zoemargerie14@gmail.com>
2026-02-20 22:53:51 +0100

    fix(main.js) : delete function loadHome

--- js/main.js
+++ js/main.js
@@ -1,18 +1,4 @@
-const projectsHome = [
-  {
-    title : "Application de recherche de stage",
-    color: "bg-stone-600 p-4 w-3/4 mx-auto rounded-xl shadow-lg shadow-stone-700",
-    image : "./assets/images/applicationStage.png",
-    description : "Cette application a été conçue pour répondre à la demande d'une cliente (fictive), souhaitant une application afin de faciliter la recherche de stage de ses étudiants.",
-  },
-  {
-    title : "DungeonXplorer",
-    color: "bg-stone-700 p-4 w-3/4 rounded-xl mx-auto shadow-lg shadow-stone-800",
-    image : "./assets/images/dungeonXplorer.png",
-    description : "DungeonXplorer est un jeu développé en PHP, il est inspiré d'un « livre dont vous êtes le héros ». Le joueur peut gérer son compte, s'inscrire, se connecter et commencer une aventure.",
-    
-  },
-]
+
 const skills = [
     {
       title : "Langages",
@@ -117,26 +103,6 @@ const interest = [
   }
   ]
 
-function loadHome(){
-  document.getElementById("projet").innerHTML = 
-  projectsHome.map(p=> `
-    <article>
-    <div class= "${p.color}">
-                <h2 class="text-2xl md:text-4xl font-bold text-center p-4 md:p-10">${p.title}</h2>
-                <img src="${p.image}"class="mx-auto rounded-xl" alt="Capture d'écran "></img>
-                <div class="md:flex md:flex-col p-4 md:p-4">
-                  <h3 class="text-2xl md:text-4xl mt-4">Description</h3>
-                  <p class="space-y-1 text-justify md:text-3xl text-l mt-4">${p.description}</p> 
-                </div>
-                <div class="flex justify-center">
-                <a href="pages/projects.html" aria-label="Voir les projets" class="hover:cursor-pointer text-center  p-4 bg-stone-800 md:w-1/2 w-full rounded-xl m-4 hover:bg-stone-900 mx-auto text-l md:text-3xl">
-                    Voir les projets</a>
-                </div>    
-              </div>
-    </article>
-
-  `).join("")
-}
 
 function loadInterests(){
   document.getElementById("interests").innerHTML = 
