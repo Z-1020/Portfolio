4434f28b75cb1c680709432d0b72068a96c30134
Z-1020 <zoemargerie14@gmail.com>
2026-02-21 02:01:41 +0100

    fix : problem menu

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
-const basePath = window.location.pathname.includes("/pages/")
-  ? "../"
-  : "./"
+
+
 document.addEventListener("DOMContentLoaded", () => {
   const mobileQuery = window.matchMedia("(max-width: 767px)");
 
@@ -14,19 +13,26 @@ document.addEventListener("DOMContentLoaded", () => {
     el.innerHTML = await res.text();
   }
   async function loadHeaderAndNavbar() {
-    await loadComponent("header", basePath + "components/header.html");
-
-const navbarPath = mobileQuery.matches
-  ? basePath + "components/telephoneNavbar.html"
-  : basePath + "components/navbar.html";
-
-  await loadComponent("navbar", navbarPath);
-  initBurgerMenu();
+    if(document.getElementById("home")){
+      await loadComponent("header", "./components/header.html");
+      const navbarPath = mobileQuery.matches
+      ? "./components/telephoneNavbarIndex.html"
+      : "./components/navbarIndex.html";
+      await loadComponent("navbar", navbarPath);
+    initBurgerMenu();
+    }
+    else{
+    await loadComponent("header", "../components/header.html");
+    const navbarPath = mobileQuery.matches
+      ? "../components/telephoneNavbar.html"
+      : "../components/navbar.html";
+       await loadComponent("navbar", navbarPath);
+    initBurgerMenu();
     }
     
-    
+  
    
-
+  }
 
   function initBurgerMenu() {
     const burger = document.getElementById("burger");
@@ -37,17 +43,26 @@ const navbarPath = mobileQuery.matches
     };
   } 
   function loadFooter(){
-   
-    fetch(basePath + "components/footer.html")
-  .then(res => res.text())
+    if(document.getElementById("home")){
+      fetch("./components/footer.html")
+      .then(res => res.text())
   .then(footerHTML => {
-    document.getElementById("footer").innerHTML = footerHTML;
-  });
+    document.getElementById("footer").innerHTML = footerHTML
+
+  })
+    }
+    else{
+    fetch("../components/footer.html")
+    .then(res => res.text())
+  .then(footerHTML => {
+    document.getElementById("footer").innerHTML = footerHTML
+
+  })
+    }
+  
 }
 loadFooter()
 
-
   mobileQuery.addEventListener("change", loadHeaderAndNavbar)
   loadHeaderAndNavbar()
-    
 });
